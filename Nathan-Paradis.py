48fa90a9d12b6865b50433d497e52a75238180bd
Felix-OlivierR <felixo.rioux@gmail.com>
2026-01-28 09:52:02 -0500

    Update Nathan-Paradis.py

--- Nathan-Paradis.py
+++ Nathan-Paradis.py
@@ -1,2 +1,11 @@
+Nathan=19
+print("How old is Nathan?")
+
+if(Nathan>18):
+    print("he's basically on his death bed")
+else:
+    print("he's freshly out the womb!"); 
+
+    print("cogrunlatis!")
 print("cogrunlatis!")
 print("whadafuk man")
