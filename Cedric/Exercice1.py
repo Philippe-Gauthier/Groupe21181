7b65aba0ad049afc222755f36b73b006703bc083
Attiepo <attiepocedric@gmail.com>
2026-02-01 19:37:04 -0500

    explication

---
+++ Cedric/Exercice1.py
@@ -0,0 +1,4 @@
+prenom = input("quel est votre prenom")
+age = input("quel est votre age")
+taille_m = input("quel est votre taille_m")
+print(f"mon prenom est{prenom},mon age est{age},ma taille_m est{taille_m}")
