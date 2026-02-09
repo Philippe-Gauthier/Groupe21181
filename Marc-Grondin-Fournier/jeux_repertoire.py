82cb9cef2efa5b9138290bfc5c9d0263432d9ca1
marcg-f <marclefournier@gmail.com>
2026-02-09 15:47:19 -0500

    up

--- Marc-Grondin-Fournier/jeux_repertoire.py
+++ Marc-Grondin-Fournier/jeux_repertoire.py
@@ -5,25 +5,33 @@ def roulette_russe():
     reset="O"
 
     while reset == "O":
+                    
                     #Sélection entre les 6 positionnements possibles du barrilet
                     choix =int(input(message.roulette_bienvenu()))
                     #Le barrilet spin pour atteindre une position au hasard
                     roll=random.randint(1, 6)
                     #Addition pour obtenir le nombre maximal de tour avant la munition
-                    roll=choix+roll                                       
+                    modu_player=choix%2
+                    modu_roll=roll%2
+                    if(choix<=roll):roll=choix+roll  
+                    elif(choix>roll):roll=choix-roll     
+
                     reset=input("Premier tour! Prêt? O/N\n")
                     reset=reset.upper()  
                     
                     for choix in range(roll):                       
                         tour_adv=choix+1 
-                        if(choix==roll): input(message.roulette_lose())      
+                        if(choix==roll): 
+                            reset=input(message.roulette_lose()) 
+                            reset=reset.upper()     
+                            break
                         elif(tour_adv==roll):
                             reset=input(message.roulette_next())
                             reset=reset.upper()
                             reset=input(message.roulette_win())
                             reset=reset.upper()                            
                             if(reset=="N"):break                           
-                        else:
+                        elif():
                              reset=input(message.roulette_adv_survi())
                              reset=reset.upper()
                              if(reset=="N"):break
--- Marc-Grondin-Fournier/message.py
+++ Marc-Grondin-Fournier/message.py
@@ -1,8 +1,10 @@
 # lib de la fonction pile ou face
 
 import random
+
 def menu():
-    return "Menu jeux \nA: Roulette russe \nB: Pile ou face \nC: Courte paille \nD: Roche, papier, ciseau \nQ: Quitter \nSélectionner un jeu!\n" 
+    return "Menu jeux \nSélectionner un jeu! \nA: Roulette russe \nB: Pile ou face \nC: Courte paille \nD: Roche, papier, ciseau \nQ: Quitter \n" 
+
 def roulette_bienvenu():
     return "Bienvenu au jeu de la roulette russe! \nSélectionner la chambre du barrilet à 6 coups pour y insérer la munition! (1,2,3,4,5,6)\n"
 def roulette_next():
@@ -10,7 +12,7 @@ def roulette_next():
 def roulette_adv_survi():
     return "Votre adversaire a survécu! À votre tour! Prêt? O/N\n"    
 def roulette_win():
-    return "Votre adversaire est mort! Rejouer? O/N\n"
+    return "Votre adversaire est mort et vous avez des couilles d'acier!! Rejouer? O/N\n"
 def roulette_lose():
     return "Oh non! Vous êtes mort! Voulez-vous réssuciter? O/N\n"
     
