ecfdda66651bb3d8d867cc1df95159b6a3546fcd
DavidCaicedo7 <davidcaicedo670@gmail.com>
2026-02-11 09:27:39 -0500

    Update Jeu.py

--- David/Jeu.py
+++ David/Jeu.py
@@ -1,5 +1,26 @@
-#contrôle:print(tu est un joueur de soccer tu as 17 ans)
 
-#entrées:input(tu choisis poste) (choisir la proposition)
+Titre = "L'ascension d'une étoile "
 
-#sorties:print(dépendant poste, propostion d'équipe sportif) (simulation de première session+ quelques choix+ équipe national)
+Introduction = "Tu as 17 ans et ce jour est celui que tu attends depuis toujours. " \
+"Après des années d'entraînement, de sacrifices et de détermination, tu te tiens au seuil de ton rêve : \
+devenir footballeur professionnel. Les recruteurs t'ont remarqué, les portes s'ouvrent, et ton destin est sur le point de basculer.Mais avant tout, qui es-tu vraiment ?"
+_1 = "ton poste sur le terrain "
+
+Choix_1_rep_1 = "Attaquant : Tu es un finisseur né. Vitesse, instinct, sang-froid devant le but. Tu vis pour marquer et faire exploser les stades."
+Choix_1_rep_2 ="Milieu de terrain : Tu es le maestro. Vision du jeu, passes décisives, contrôle du tempo. Tu orchestres chaque attaque."
+
+
+
+print (Titre)
+print(Introduction)
+
+"""
+def formater_question(Titre, Introduction):
+    page = f"Vous devez répondre à la question suivante:{Titre}"
+    page = page +"/n" +"____________________________________"
+    page = page +"/n" +"vous devez choisir parmis ces choix"
+    page = page +Introduction
+    return page 
+
+choix = input(formater_question(Choix_1, Choix_1_rep_1, Choix_1_rep_2))
+"""
