5c90ad81d749ecfadae269309513b7f2432e3476
Attiepo <attiepocedric@gmail.com>
2026-02-11 11:14:46 -0500

    Projet 1
    Début du projet

--- Cedric/Cedric.py
+++ Cedric/Cedric.py
@@ -16,23 +16,74 @@ def réponse (choixA, choixB, choixC, vrai_reponse) :
 print("niveau_1 Capital des pays")
 print("---------------------------")
 
-print ("1- Quelle est la capital du Canada")
+print ("1- Quelle est la capital du Canada ?")
 réponse ("Otawa", "Toronto", "Vancouver", "1")
 
-print ("2- Quelle est la capitale de la France")
+print ("2- Quelle est la capitale de la France ?")
 réponse ("Lyon", "Paris", "Marseille", "2")
 
-print ("3- Quelle est la capitale du Japon")
+print ("3- Quelle est la capitale du Japon ?")
 réponse ("Kyoto", "Osaka", "Tokyo", "3")
 
-print ("4- Quelle est la capitale de l'Italie")
+print ("4- Quelle est la capitale de l'Italie ?")
 réponse ("Vénise", "Rome", "Milan", "2")
 
-print ("5- Quelle est la capitale de l'Allemagne")
+print ("5- Quelle est la capitale de l'Allemagne ?")
 réponse ("Berlin", "Hambourg", "Munich", "3")
 
-print ("6- Quelle est la capitale de l'Espagne")
+print ("6- Quelle est la capitale de l'Espagne ?")
 réponse ("Madrid", "Barcelone", "Valence", "1")
+
+print ("7- Quelle est la capitale du portugal ?")
+réponse ("Porto", "Lisbonne", "Faro", "2")
+
+print ("8- Quelle est la capitale du Brésil ?")
+réponse ("Brasilia", "Sao paulo", "Rio de janeiro", "1")
+
+print ("9- Quelle est la capitale de l'Anglterre ?")
+réponse ("Manchester", "Liverpool", "Londres", "3" )
+
+print ("10- Quelle est la capitale de la Cote - d'Ivoire")
+réponse ("Abidjan","Yamoussoukro", "Abobo Doumé" "2")
+
+print ("11- Quelle est la capitale du Maroc ?")
+réponse ("Casablanca", "Marraketch", "Rabat", "3")
+
+print ("12- Quelle est la capitale de la Chine ?")
+réponse ("Shanghai","Pékin","Hong Kong", "2")
+
+print ("13- Quelle est la capitale de la Russie ?")
+réponse ("Moscou", "Kazan", "Saint-Pétersbourg", "1")
+
+print ("14- Quelle est la capitale du Mexique ?")
+réponse ("Cancun", "Yopougon", "Mexico", "3")
+
+print ("15- Quelle est la capitale de l'Argentine ?")
+réponse ("Koumassi", "Buenos Aires", "Treichville", "2")
+
+print ("16- Quelle est la capitale du Sénégal ?")
+réponse ("Saint-Louis", "Dakar", "Yopougon(Ghandi)", "2")
+
+print ("17- Quelle est la capitale de la Suisse ?")
+réponse ("Zurich", "Génève", "Berne", "3")
+
+print ("18- Quelle est la capitale de la Norvège ?")
+réponse ("Olso", "Bergen", "Trondheim", "1")
+
+print ("19- Quelle est la capitale de la Belgique ?")
+réponse ("Bruxelle", "Anvers", "Liège", "1")
+
+print ("20- Quelle est la capitale du Ghana ?")
+réponse ("Port Bouet", "Abobo Doumé", "Accra", "3")
+
+#Niveau 2 du jeu de quiz educatif: Mathématiques
+
+print("niveau_2 Capital des pays")
+print("---------------------------")
+
+
+
+
 
 
 
