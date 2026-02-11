a751a7e19c34cf1951f9522225c3140a6278bd7f
Arnaud-Baril <insommelierswetrust@gmail.com>
2026-02-11 09:01:53 -0500

    change

---
+++ Arnaud/Projet 1/Jeu.py
@@ -0,0 +1,7 @@
+#Fait par Arnaud Baril TGE G.21181
+
+import Texteprojet1
+
+Print(texte1)
+Print("nope")
+Input()
---
+++ Arnaud/Projet 1/Texteprojet1.py
@@ -0,0 +1,7 @@
+texte1 = """Whaaaa! whaaaaaaa! fait le bébé.
+
+... 
+
+Tu regardes ta femme commencer a perdre patience, pour la quatrième fois se soir, avec le bébé.
+
+Ta décision est prise. Tu vas aller chercher du lait et des cigarettes au dépanneur."""
