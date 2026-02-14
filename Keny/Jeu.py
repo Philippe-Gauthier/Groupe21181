a8233330b9582873e0f81abd042fd100fdbeb8b3
kenyboka <keny.boka1@gmail.com>
2026-02-14 17:49:27 -0500

    Update Jeu.py

--- Keny/Jeu.py
+++ Keny/Jeu.py
@@ -11,23 +11,24 @@ def demander_choix(choix_1,choix_2):
     But: Demander a l'utilisateur de faire un choix
     """
 
-    choix = input("Vous pouvez entrez votre choix svp 1(choix_1 ou choix_2)")
-    print("------------------------------")
+    choix = int(input("Vous pouvez entrez votre choix svp (`{choix_1} ou {choix_2})"))
+    print("-" * 50)
     print(f"Joseph est un étudiant en électronique programmable")
     print(f"Joseph a pour ambition de réussir sa session")
     print(f"{choix_1}- joseph gere bien son horaire")
     print(f"{choix_2}- joseph néglige son horaire")
-    
-    if choix == "choix_1":
+    if choix == choix_1:
         print("Joseph gere bien son horaire")
-    else:
+    elif choix == choix_2:
         print("Joseph néglige son horaire")
-    return choix
+    else:
+        print("choix invalid")
+    return choix  
 
 demander_choix(1,2)
 
 print(f"Joseph gere bien son horaire")
-if demander_choix == "choix_3":
+if demander_choix == 3:
         print(f" joseph arrive a l'heure")
 else:
         print(f"Joseph arrive souvent en retard")
