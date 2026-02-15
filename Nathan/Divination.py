ba0c19bb76fef975495197e8381612171323ca6d
NathanParadis-72 <paradisnathan72@gmail.com>
2026-02-15 15:44:31 -0500

    Update Divination.py

--- Nathan/Divination.py
+++ Nathan/Divination.py
@@ -66,20 +66,23 @@ lvl1 = [text1, option1_1, option1_2, option1_3]
 
 #definition des fonctions du jeu
 
+
+#petite fonction pour print des lettres une a une (certains problemes de formattage, seuelement utiliser pour des petites strings)
+#utlisée ici pour "l'animation" de loading, changer le time.sleep a environ 0.008 pour des strings de texte
+def print_par_lettre(text):
+    for caracteres in text:
+        print(f"{caracteres}", end="", flush=True)
+        time.sleep(0.4)
+    print("\n\n")
+
+
+
 #fonction pour demander au jouer s'il est pret a commencer le jeu
 def start_game():
     while True:
         start_confirmation = input("Pret a jouer? (y/n) ")
         if start_confirmation == "y":
-            print("Alors c'est parti!")
-            time.sleep(0.5)
-            print(f"\r.", end="")
-            time.sleep(0.7)
-            print(f"\r..", end="")
-            time.sleep(0.9)
-            print(f"\r...", end="")
-            time.sleep(1.2)
-            print("\n")
+            print_par_lettre(".....") #fonction utilisée ici pour le "loading"
             break
         elif start_confirmation == "n":
             print("Ah bon, au revoir alors")
@@ -90,16 +93,16 @@ def start_game():
 
 
 
-#module d'extraction d'information de niveau
+#module d'extraction d'information de niveau, à utiliser en duo avec la prochaine fonction
 def level_extractor(niveau_selectionne):
-    variable_test0 = niveau_selectionne[0]
-    variable_test1 = niveau_selectionne[1]
-    variable_test2 = niveau_selectionne[2]
-    variable_test3 = niveau_selectionne[3]
-    return variable_test0, variable_test1, variable_test2, variable_test3
+    situation0 = niveau_selectionne[0]
+    choix1 = niveau_selectionne[1]
+    choix2 = niveau_selectionne[2]
+    choix3 = niveau_selectionne[3]
+    return situation0, choix1, choix2, choix3
 
 
-#module de présentation des choix
+#module de présentation des choix, utiliser avec la fonction ci dessus
 def presentation_choix(situation0, choix_1, choix_2, choix_3):
     time.sleep(0.5)
     print(f"{situation0}\n")
@@ -114,7 +117,7 @@ def presentation_choix(situation0, choix_1, choix_2, choix_3):
     return reponse
 
 
-#module d'assignation de valeur au résultat du choix
+#module d'assignation de valeur au résultat du choix, prends le résultat de la fonction ci dessus et lui assigne une valeur fixe
 def resultat_choix(reponse):
     if reponse == 1:
         choix_final = 1
@@ -130,12 +133,12 @@ def resultat_choix(reponse):
     return choix_final
 
 
-#fonction de "looping" du code au cas ou la réponse n'est pas valide
+#combinaison de toutes les fonctions ci dessus sauf les 2 premieres, + fonction de "looping" du code au cas ou la réponse n'est pas valide 
 def loop_niveau(input_niveau):
     while True:
 
-        situation0, description1, description2, description3 = level_extractor(input_niveau)
-        reponse_user = presentation_choix(situation0, description1, description2, description3)
+        situation0, choix1, choix2, choix3 = level_extractor(input_niveau)
+        reponse_user = presentation_choix(situation0, choix1, choix2, choix3)
 
         verif_reponse = resultat_choix(reponse_user)
         if verif_reponse == 4:
@@ -154,7 +157,7 @@ def loop_niveau(input_niveau):
                 print("Bon, tu parles chinois maintenant, j'imagine qu'on continue alors...")
                 time.sleep(1.5) #retour aux choix
         elif verif_reponse != 4:
-            prochain_niveau = verif_reponse #passer au prochain niveau
+            prochain_niveau = verif_reponse #donner l'indication pour le prochain niveau
             break
 
     return prochain_niveau
@@ -179,10 +182,13 @@ if niveau == 1:
     niveau2 = loop_niveau(prochain_niveau)
     if niveau2 == 1:
         prochain_niveau = lvl1_1
+        niveau2 = loop_niveau(prochain_niveau)
     elif niveau2 == 2:
         prochain_niveau = lvl1_2
+        niveau2 = loop_niveau(prochain_niveau)
     elif niveau2 == 3:
         prochain_niveau = lvl1_3
+        niveau2 = loop_niveau(prochain_niveau)
 
 
 elif niveau == 2:
@@ -190,10 +196,13 @@ elif niveau == 2:
     niveau2 = loop_niveau(prochain_niveau)
     if niveau2 == 1:
         prochain_niveau = lvl1_1
+        niveau2 = loop_niveau(prochain_niveau)
     elif niveau2 == 2:
         prochain_niveau = lvl1_2
+        niveau2 = loop_niveau(prochain_niveau)
     elif niveau2 == 3:
         prochain_niveau = lvl1_3
+        niveau2 = loop_niveau(prochain_niveau)
 
 
 elif niveau == 3:
@@ -201,10 +210,13 @@ elif niveau == 3:
     niveau2 = loop_niveau(prochain_niveau)
     if niveau2 == 1:
         prochain_niveau = lvl1_1
+        niveau2 = loop_niveau(prochain_niveau)
     elif niveau2 == 2:
         prochain_niveau = lvl1_2
+        niveau2 = loop_niveau(prochain_niveau)
     elif niveau2 == 3:
         prochain_niveau = lvl1_3
+        niveau2 = loop_niveau(prochain_niveau)
 
 
 
