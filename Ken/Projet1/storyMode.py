98f258cbc602db5aa54b6d0a229fe2a3fb551da5
Ken Lopez <lopk99@hotmail.fr>
2026-02-18 08:34:13 -0500

    Update storyMode.py
    Code Update

--- Ken/Projet1/storyMode.py
+++ Ken/Projet1/storyMode.py
@@ -69,7 +69,7 @@ def dice_Check():
             result_dice = int(result_dice)
             if result_dice >= 1 and result_dice <= 6:
                 break
-            if result_dice == 7:
+            elif result_dice == 7:
                 secret_ending()
             else:
                 print("Impossible, un dé a seulement 6 faces ;)")
@@ -78,6 +78,7 @@ def dice_Check():
         result_dice = roll_dice()
     return result_dice
 
+#Petites fonctions supplémentaires
 def yes_no():
     """
     ENTRÉE : input d'utilisateur (string)
@@ -98,13 +99,17 @@ def secret_ending():
     """
     ENTRÉE : RIEN
     SORTIE : RIEN
-    BUT :    FIN SECRÈTE AMUSANTE
+    BUT :    FIN SECRÈTE AMUSANTE si l'utilisateur entre 7 comme résultat de dé. Accessible seulement si tu as le dé
     """
     print_slow("-Carraway! commença le détective. Je... enfin... euhmmm... \n-Je m'occupe de le faire parler cette fois? Dit Carraway pour aider à terminer la phrase de son collègue.\n\n-COUPEZ! \n-Meeeerde désolé les gars j'ai eu un p'tit blanc de mémoire." \
     "\n-Mais merde t'es sérieux? T'as gâché la prise! On doit tout recommencer maintenant! Dit le directeur de tournage\n-T'inquiète ça arrive. Dit l'acteur qui joue Carraway. T'es nouveau dans le showbiz.\n-Merci mec, désolé j'suis nerveux.\n-Ça va\n-Allez! On se prépare.\n\n" \
     "Pendant que l'acteur se répetait les lignes pour ne pas les oublier, l'acteur de Carraway se regarda pendant un instant au miroir. Il sourit à son reflet. Il ne voulait pas brimer son moral. Il savait comment tout ceci terminait et il essayait de garder une bonne humeur malgré tout\n\n" \
     "-BON ON SE PRÉPARE! Ce que l'autre a dit, Scène 2, ACTION!\n\n-Asseyez-vous droit, Elliot. Ça ira plus vite si on se parle calmement....\n\n\nFIN...?")
-    
+
+    exit()
+
+#Fonctions de dialogue
+def save_ending5():
     return False
 
 #Variables de santé mentale pour la fin
@@ -135,7 +140,6 @@ print_slow("Ce que l'autre a dit")
 time.sleep(2)
 clear_screen()
 
-print_slow("Chapitre 1 : L'interrogatoire")
 clear_screen()
 print_slow("9 octobre 1936, ??H?? AM \n\n\nLa pièce était trop petite pour respirer correctement.\nUne ampoule nue pendait au plafond, oscillant lentement, projetant des ombres instables sur les murs.\n\nEliot Kramer restait immobile "\
 "sur sa chaise.\nDevant lui, une table en métal et une tasse de café froide. L'odeur de cigarette s'était incrustée dans la pièce depuis longtemps\n\nIl n'avait pas touché à la tasse.\nIl n'avait pas parlé non plus.\nLa porte s'ouvrit."\
@@ -162,7 +166,7 @@ elif roll_result %2 != 0:
 
 if controle == 1:
     print_slow("-Écoutez... Commença le suspect. Je ne suis pas plus au courant que vous. En fait je ne sais pas ce que je fait ici...\n\n-Il ment. Dit l'inspecteur aux bras croisés. Regarde ses mains, ils tremblent comme des putains de feuilles mortes.\n" \
-    "L'agent Carraway baissa les yeux et vit effectivement des mains tremblantes sur la table. Eliot essayait de cacher sa nervosité... Carraway avait honte de ne pas l'avoir remarqué en premier que le détective Buchanan. ")
+    "L'agent Carraway baissa les yeux et vit effectivement des mains tremblantes sur la table. Eliot essayait de cacher sa nervosité... Carraway se détestait de ne pas l'avoir remarqué en premier que son collègue, le détective Buchanan. ")
 elif violence == 1:
     print_slow("-PARLE! Cria Buchanan en frappant du poing sur la table.\n\nL'autre détecctive se raidit d'un coup au son métallique de la table.\n\n-Calme toi, Carraway. Je ne lui ai rien fait. Pas encore. Dit Buchanan avec un sourire malicieux. Eliot ressentait " \
     "une sueur froide qui lui coulait le long du dos.\n\n-Je...je ne sais pas de quoi tu me parles...je..je..je n'ai rien à voir dans tout ça.\n\n\n-Il est nerveux, dit Carraway. Donne lui un peu d'espace. Tu l'as assez joué sur la peur.\n-Non, il ment cet enculé...\n\n")
@@ -175,9 +179,9 @@ if roll_result <= 2:                    #Option pacifique
     controle += 1
     lucidite += 1
     if violence == 1 :
-        print_slow("-Allez c'est bon je vais continuer. Dit l'agent Carraway.\nBuchanan gloussa. Il baissa la tête de déception, et recula.\n\n ")
+        print_slow("-Allez c'est bon je vais continuer. Dit l'agent Carraway.\nBuchanan gloussa. Il baissa la tête, la secoua de déception, et recula.\n\n ")
 
-    print_slow("-Regarde Kramer. Ici, ce que toi et moi, on veut est simple. Tu veux sortir au plus vite et moi, je veux trouver le coupable pour l'amener en justice. Mais on sait que tu as des informations utiles. Et si tu continues à éviter de répondre à la question, alors "\
+    print_slow("-Regarde Kramer. Ici, ce que toi et moi on veut est simple. Tu veux sortir au plus vite et moi, je veux trouver le coupable pour l'amener en justice. Mais on sait que tu as des informations utiles. Et si tu continues à éviter de répondre à la question, alors "\
                "mon collègue sera fâché. Je dois assez le supporter dans la journée. Alors ne rends pas la tâche plus difficile.\n-Quoi? Dit le suspect d'un air confus.\n-Allez. Ne perdons pas plus de temps. Le jour du meutre, on vous a vu à quelques rues de la scène de crime." \
                "Que faisiez-vous et où alliez-vous?\n\nKramer soupira un long coup")
 elif roll_result <= 4:                  #Option neutre
@@ -193,7 +197,7 @@ elif roll_result<= 6:                   #Option violente
     "non plus, car sinon, il sait qu'il passera un terrible moment.\n\nL'autre détective ne perdit pas de temps pour utiliser ses poings. Il souleva le suspect par le col, lui adressa un solide coup de tête qui le fit vasciller vers l'arrière. Il en profita alors pour lui offrir un direct " \
     "sur l'estomac pour enfin le plaquer contre le mur\n\n-Ok! Ok! Je vais vous dire! Merde! Arrêtez s'il-vous plaît! \n-Bon tu vois? C'était pas si difficile que ça pas vrai? Il assit alors Kramer sur la chaise.\n\n")
 
-print_slow("-Vous avez deja lu les lettres? Demanda Elliot.\n-Lesquelles? Demanda sèchement Buchanan.\n-Celles qu'on ne montre pas aux journaux.\n\n-De quoi il parle encore?\n\n-Je... j'en suis pas au courant. Dit Carraway.\n\n")
+print_slow("-Est-ce que vous avez deja lu les lettres? Demanda Elliot.\n-Lesquelles? Demanda sèchement Buchanan.\n-Celles qu'on ne montre pas aux journaux.\n\n-De quoi il parle encore?\n\n-Je... je pense savoir ce qu'il essaie de dire. Dit Carraway.\n\n")
 
 
 #Choix crucial #3
@@ -223,20 +227,23 @@ if simple_choice == "NON":                      #Approche de Fin 4 et 5 car pas 
 
             print_slow("-Je tiens à vous dire que, depuis que vous êtes entré dans la salle, vous n'avez jamais regardé dans votre 'dossier' de police.\n-Ah ouais? Pas besoin car je le connais par coeur mon dossier, moi! Je suis avec le NYPD comme tu peux le voir sur mon badge. Dit le détective en touchant son badge de police.\n\n")
             lucidite += 3
-            print_slow("En fait, il n'y avait qu'une épinglette souvenir de New York\n\n-Quoi? Je ne comprends pas. Où est mon badge?!\n\n-Vous n'êtes plus vous-même, détective.\n\nLe détective se tourna pour voir son collègue, qui était silencieux depuis un moment...\n\n")
+            print_slow("\n\n-Quoi? Je ne comprends pas. Où est mon badge?! Je l'avait sur moi en entrant ici! Redonne-le moi, voyou de merde\n\n-Vous n'êtes plus vous-même, détective.\n\nLe détective se tourna pour voir son collègue, qui était silencieux depuis un moment...\n\n")
             roll_result = dice_Check()
             if roll_result % 2 == 0:
                 print_slow("Son collègue était bien là. Confus pour toute cette situation.\nMais ne disait aucun mot. ")
             else:
-                print_slow("Il n'y avait personne d'autre dans la salle à part lui et le suspect. Il ne se rappelait plus du nom du suspect. Il a sûrement été drogué. Il a voulu sortir son arme pour se défendre, " \
-                "mais il sortit à la place une banane.\n\n")
-            print_slow("Il n'en revenait pas. La salle était devenue blanche. Le miroir n'était plus là. Le suspect se leva et dit : Bon on peut maintenant jeter les rideaux. Ça ne fonctionne pas du tout. VOUS POUVEZ ENTRER!.\nÀ peine ces mots dit, " \
-            "des personnes à blouse blanche sont entrés avec une veste de force.\n-Je suis désolé, M. Carraway. Nous avons vraiment essayé. Je sais que votre ancien métier vous manque,alors on s'était dit que le dossier que vous avez jamais réussi à résoudre vous aiderait." \
-            "ce sera alors pour une autre fois. \n\nCarraway essaya de se débattre. Il était un inspecteur, enfin, il pense. Les infirmiers lui injectèrent un sédatif. Et l'amènèrent dans un couloir blanc, vide. À tout jamais.\n\nFIN.")
-
-
-    else:
-        print_slow("Ouais on n'a pas ")
+                print_slow("Il n'y avait personne d'autre dans la salle à part lui et le suspect. Il ne se rappelait plus du nom du suspect. Il a sûrement été drogué. Il sortit son arme pour se défendre. " \
+                "Arme pointée sur le suspect, il ne résagissait pas plus. C'est alors que Carraway comprit qu'une banane n'était aucunement menacante.\n\n")
+            print_slow("Il n'en revenait pas. La salle était devenue blanche. Le miroir n'était plus là. Le suspect se leva et dit : Bon on peut maintenant jeter les rideaux. Ça ne fonctionne pas du tout. Il cria : VOUS POUVEZ ENTRER!.\nÀ peine ces mots dit, " \
+            "des personnes à blouse blanche sont entrés avec une veste de force.\n-Je suis désolé, M. Carraway. Nous avons vraiment essayé. Je sais que votre ancien métier vous manque, alors on s'était dit que le dossier que vous avez jamais réussi à résoudre vous aiderait." \
+            "ce sera alors pour une autre fois. \n\nCarraway essaya de se débattre. Il était un détective, enfin, il pense. Les infirmiers lui injectèrent un sédatif. Et l'amènèrent dans un couloir blanc, vide. À tout jamais.\n\nFIN.")
+        else :
+            save_ending5()
+    else:                                       #Sinon, FIN 5 (la vraie fin, la vraie de vrai)
+        print_slow("-Là vous ne répondez pas aux attentes M. Kramer. Avez-vous, oui ou non vu quelqu'un ou quelque chose de suspect?\n-Vous le savez deja.\n-Quoi?\n-Vous savez qui a commis ces horreurs, mais vous refusez de l'admettre.\n\nBuchanan sorti une cigarette. Il comprit que des aveux se préparaient à sortir. Lesquelles? C'était sans importance. Il ne voulait que la justice." \
+        "Chaque expiration de la cigarette de Buchanan faisait tousser Carraway. Il détestait l'odeur.\n\n-Eh bien, quand je me disais que c'était juste un petit crime... Qu'as-tu fait Carraway?\n-Rien, je n'ai aucune idée de quoi il parle.\n-Menteur.\n-Et vous, M. Elliot, je vous rapelle que vous êtes le premier suspect dans l'affaire. Alors arrêtez de tourner" \
+        "l'investigation vers moi. Tu ne réussiras pas à me manipuler.\n-Ok je vais dire un truc. Oui j'ai vu quelqu'un. Cette person portait un chapeau élégant mais il avait les mains tachés de sang. Je l'ai vu sortir de la ruelle où que le crime a été commis." \
+        "que je fais parti des forces de l'ordre! Je n'ai ")
 
 elif simple_choice == "OUI":                    #Approche de Fin 1 2 3 et 6
     corruption += 2
@@ -255,8 +262,8 @@ violence   ≥ 10
 FIN 3 --capitaine mechant
 corruption ≥ 8
 
-FIN 4 --Asylum
-lucidite   ≥ 10
+-------------------------------------------------------FIN 4 --Asylum--------------------------------------------------------------------------
+-------------------------------------------------------lucidite   ≥ 10-------------------------------------------------------------------------             
 
 FIN 5 --Real ending
 realite    ≥ 6
@@ -264,11 +271,5 @@ realite    ≥ 6
 FIN 6 --sujet clos sans savoir
 mensonge   ≤ 6
 
-FIN 7 --?????
-controle   ≥ 6
-violence   ≥ 6
-lucidite   ≥ 6
-mensonge   ≤ 6
-realite    ≥ 6
-corruption ≤ 6
+--------------------------------------------------------FIN 7 --?????--------------------------------------------------------------------------
 """
