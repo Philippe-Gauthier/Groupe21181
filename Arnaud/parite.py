7ce063878ffe84ade38a586f542a8d9c57bc095a
Arnaud-Baril <insommelierswetrust@gmail.com>
2026-02-25 10:38:37 -0500

    Version Super Finale

--- Arnaud/parite.py
+++
@@ -1,7 +0,0 @@
-nbentier = int(input("rentre un nomber entier: "))
-if 1 == nbentier%2 :
-    print("impair")
-elif 0 == nbentier%2 :
-    print("pair")
-else:
-    print("fail")
--- Arnaud/vitesse.py
+++
@@ -1,6 +0,0 @@
-def kmh_vers_mph(kmh):
-    mph = kmh / 1.609
-    return mph
-
-kmh = float(input("Quelle est la vitesse a convertir? "))
-print(kmh_vers_mph(kmh))
