f18954d9cb534dc6173fe52e116d0788e0527886
Arnaud-Baril <insommelierswetrust@gmail.com>
2026-02-25 08:59:15 -0500

    temporaire

--- Arnaud/Projet 1/Jeu.py
+++ Arnaud/Projet 1/Jeu.py
@@ -1,48 +1,140 @@
-"""Fait par Arnaud Baril TGE G.21181"""
+"""
+nom du jeu:
+Auteur: Arnaud Baril TGE G.21181
+
+(identifier dans le commit final sur la main branche que c<Est la bonne version)
+"""
 
 import Texteprojet1
-
+#IMPORTANT! LIRE LE DOCUMENT DES STANDARDS DE CODE
+RETRY = True
 choix = 0
 
+"""
+entree de la fonction:
+sortie de la fonction:
+but de la fonction:
+"""
 def prochainepage(texte):
     print(texte)
     choix = 0
     while choix != "1" and  choix != "2":
-       choix = (input("ecrire 1 ou 2\n"))
+       choix = (input("Votre choix:  "))
     choix = int(choix)
     return choix
 
-print(Texteprojet1.page1)
-input("Suivant")
+def lire_la_suite(page)
+    print(page)
+    input("peser entrer pour la suite")
+    return
 
-print(Texteprojet1.page1_1)
-input("suivant")
 
-choix = prochainepage(Texteprojet1.page1_12)
-if choix == 1:
-    choix = prochainepage(Texteprojet1.page2)
+while  RETRY == True:
+
+        
+    lire_la_suite(Texteprojet1.page_debut)
+
+    lire_la_suite(Texteprojet1.page_debut1)
+
+    choix = prochainepage(Texteprojet1.page_debut2)
     if choix == 1:
-        choix = prochainepage(Texteprojet1.page3)
-        Choix = prochainepage(Texteprojet1.page3_1)
-    elif choix == 2:
-          choix = prochainepage(Texteprojet1.page4) 
-if choix == 2:
-    choix = prochainepage(Texteprojet1.page5)
-    if choix == 1:
-        choix = prochainepage(Texteprojet1.page6)
-    if choix == 2:
-        choix = prochainepage(Texteprojet1.page7)
+        choix = prochainepage(Texteprojet1.page_dance)
         if choix == 1:
-            choix = prochainepage(Texteprojet1.page8)
+            choix = prochainepage(Texteprojet1.page_danse_robot)
+            Choix = prochainepage(Texteprojet1.page_robot_break)
+        elif choix == 2:
+            choix = prochainepage(Texteprojet1.page_gige) 
+    if choix == 2:
+        choix = prochainepage(Texteprojet1.page_heehee)
+        if choix == 1:
+            choix = prochainepage(Texteprojet1.page_tamiat)
         if choix == 2:
-            choix = prochainepage(Texteprojet1.page9)
+            choix = prochainepage(Texteprojet1.page_autopsie)
             if choix == 1:
-                choix == prochainepage(Texteprojet1.page11)
-                
+                choix = prochainepage(Texteprojet1.page_glandis)
             if choix == 2:
-                choix = prochainepage(Texteprojet1.page10)
-
-
-              
-else:
-    print("fail")
+                choix = prochainepage(Texteprojet1.page_kazakstan)
+                if choix == 1:
+                    choix = prochainepage(Texteprojet1.page_politique)
+                    if choix == 1:
+                        choix = prochainepage(Texteprojet1.page_coeur)
+                        choix = prochainepage(Texteprojet1.page_reptilien)
+                        if choix == 1:
+                            choix = prochainepage(Texteprojet1.page_beurre)
+                            if choix == 1:
+                                choix = prochainepage(Texteprojet1.page_baba)
+                            if choix == 2:
+                                choix = prochainepage(Texteprojet1.page_again)
+                        if choix == 2: 
+                            choix = prochainepage(Texteprojet1.page_portal)
+                            if choix == 1:
+                                choix = prochainepage(Texteprojet1.page_beurre)
+                            if choix == 2:
+                                choix = prochainepage(Texteprojet1.page_double)
+                                if choix == 1:
+                                    choix = prochainepage(Texteprojet1.page_retour)
+                                    choix = prochainepage(Texteprojet1.page_retour2)
+                                if choix == 2:
+                                    choix = prochainepage(Texteprojet1.page_dopleganger)
+                            if choix == 2:
+                                choix = prochainepage(Texteprojet1.page_jurassic)
+                                if choix == 1:
+                                    choix = prochainepage(Texteprojet1.page_explication)
+                                    if choix == 1:
+                                        choix = prochainepage(Texteprojet1.page_space_jesus)
+                                        choix = prochainepage(Texteprojet1.page_nouveau_monde)
+                                        if choix == 1:
+                                            choix = prochainepage(Texteprojet1.page_anglais)
+                                            if choix == 1:
+                                                choix = prochainepage(Texteprojet1.page_magie)
+                                                choix = prochainepage(Texteprojet1.page_magie_chat)
+                                                if choix == 1:
+                                                    choix = prochainepage(Texteprojet1.page_magie_portail)
+                                                    choix = prochainepage(Texteprojet1.page_monologue)
+                                                    choix = prochainepage(Texteprojet1.page_wtf)
+                                            if choix == 2:
+                                                choix = prochainepage(Texteprojet1.page_hero)
+                                                if choix == 1:
+                                                    choix = prochainepage(Texteprojet1.page_hero_bad_sword)
+                                                if choix == 2:
+                                                    choix = prochainepage(Texteprojet1.page_hero_operation)
+                                                    if choix == 1:
+                                                        choix = prochainepage(Texteprojet1.page__hero_quest)
+                                                    if choix == 2:
+                                                        choix = prochainepage(Texteprojet1.page_glandalf)
+                                                if choix == 2:
+                                                    choix = prochainepage(Texteprojet1.page_magie_combat)
+                                            if choix == 2:
+                                                choix = prochainepage(Texteprojet1.page_hero)
+                                        if choix == 2:
+                                            choix = prochainepage(Texteprojet1.page_boude)
+                                    if choix == 2: 
+                                        choix = prochainepage(Texteprojet1.page_nobel)
+                                        if choix == 1:
+                                            choix = prochainepage(Texteprojet1.page_noel)
+                                            if choix == 1:
+                                                choix = prochainepage(Texteprojet1.page_nomnom_divin)
+                                            if choix == 2:
+                                                choix = prochainepage(Texteprojet1.page_retour_space_jesus)
+                                        if choix == 2:
+                                            choix = prochainepage(Texteprojet1.page_prout)        
+                                if choix == 2:
+                                    choix = prochainepage(Texteprojet1.page_rein)
+                                    if choix == 1:
+                                        choix = prochainepage(Texteprojet1.page_dinogarou)
+                                        if choix == 1:
+                                            choix = prochainepage(Texteprojet1.page_fin_rein)
+                                        if choix == 2: 
+                                            choix = prochainepage(Texteprojet1.page_rein_garou)
+                                    if choix == 2:
+                                        choix = prochainepage(Texteprojet1.page_boom)
+                    if choix == 2:
+                        choix = prochainepage(Texteprojet1.page_oeil)
+                        
+                    if choix == 2:
+                        choix = prochainepage(Texteprojet1.page_borat)
+                if choix == 2:
+                    choix = prochainepage(Texteprojet1.page_borat)
+             
+    else:
+        print("Essayer de nouveau")
--- Arnaud/Projet 1/Jouer.bat
+++ Arnaud/Projet 1/Jouer.bat
@@ -1,7 +1,4 @@
-REM echo off permet de ne pas voir ce que command fait.
 @echo off
-REM permet de choisir la maximum de ligne et collonnes utiliser. Standardise pour ne pas deformé le texte et image.
-mode con: cols=50 lines=30
-REM ouvre le jeu lui même.
-python jeu.py
-pause
+REM echo off permet de ne pas voir ce que command fait.
+REM ouvre le jeu lui même avec l'écran maximiser.
+start "" /max cmd /k python Jeu.py
