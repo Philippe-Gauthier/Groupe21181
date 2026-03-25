cfbbf00cbca9b83c94a57637e317952760288d66
Felix-OlivierR <felixo.rioux@gmail.com>
2026-03-25 10:34:54 -0400

    Ajout de prise de note, méthode pour noter les exam et print le tout. Le log fonctionne aussi
    Commit mon projet pour pas rien perdre

--- Félix-Olivier_R/Projet 2/FONCTIONSP2.py
+++ Félix-Olivier_R/Projet 2/FONCTIONSP2.py
@@ -1,49 +1,89 @@
-import sys, time
+import datetime 
+from time import sleep
 
 '''
 Projet 2 Lecture et écriture de fichiers FICHIER DE FONCTIONS :)
 Félix-Olivier rioux
 '''
 
-'''
-variable = open(" 'nom du fichier' ",   " '_' ")
-r = Read
-w = Write
-a = Add
-x = Create
-'''
 
-
-
-
-def logs(input):
+def Document(append,file,log):
     '''
-Entrée: input à rentrer dans les logs
-Sortie: Dans le fichié Projet2logs, ajout de ligne
-'''
+    Entrée: append = info choisi par l'utilisateur. file = choisir le fichier où écrire. log = choisir où logger l'info
+    Sortie: Dans le fichié chooisi, ajout de ligne avec l'info et le datetime
+    '''
+    time=datetime.datetime.now()
     while True:
         try:
-            logs = open("Projet2logs.txt", "x")
+            logs = open(f"{log}.txt", "x")
             logs.close
-        except: 
-          
-            logs = open("Projet2logs.txt", "a")
-            logs.write(f"{input}")
+        except FileExistsError: 
+            logs = open(f"{log}.txt", "a")
+            logs.write(f"[Try create file - {log} - : {time}]\n")
+            logs.write(f"[ERROR: Fichier existant - {log} - : {time}]\n")
+            logs.close
+            logs = open(f"{log}.txt", "a")
+            logs.write(f"[- {append} - was added to {file} : {time}]\n")
+            while True:
+                try:
+                    fichier=open(f"{file}","x")
+                    fichier.close
+                except FileExistsError:
+                    fichier=open(f"{file}","a")
+                    fichier.write(f"{append}    -   ")
+                    break
             break
+    fichier.close
     logs.close
 
-def choice_num(): 
-    """
-    Entrée: Nombre (ou pas) entré par lutilisateur
-    Sortie: Un numéro en format INT, entrée par l'utilisateur, ou un erreur
-    But: S'assurer que l'utilisateur rentre un nombre pour pouvoir l'affecter avec des maths.
-    """
-    while True: 
-        number=input("Veuillez entrez un nombre : ") 
+def dateexam(cours,date,info,log):
+    time=datetime.datetime.now()
+    fichier=open(f"{cours}.csv","a")
+    while True:
         try:
-            result=int(number)
-            break
-        except ValueError:
-            print("\nInvalide")
-    return result
+            logs = open(f"{log}.txt", "x")
+            logs.close
+        except FileExistsError: 
+            while True:
+                try: 
+                    fichier=open(f"{cours}.csv","x")
+                    fichier.close
+                except FileExistsError:
+                        logs = open(f"{log}.txt", "a")
+                        logs.write(f"[In {cours} added {date},{info} {time}]\n")
+                        logs.close
+                        fichier=open(f"{cours}.csv","a")
+                        fichier.write(f"{date},{info}")
+                        fichier.close
+                        break
+        break
+                
+
+def lecture(cours):
+    try:
+        lecturetxt=open(f"{cours}.txt","r")
+        lecturecsv=open(f"{cours}.csv","r")
+        print(f"\nNotes -------------------\n {lecturetxt.readlines()}")
+        csv=lecturecsv.readlines()
+        dateinfo=[]
+        for ligne in csv:
+            ligne=ligne.strip()
+            date = ligne.split(",")
+            dictionnaire = {
+                "jj": date[0],
+                "mm": date[1],
+                "aaaa": date[2],
+                "info": date[3],
+            }
+            dateinfo.append(dictionnaire)
+        for i in dateinfo:
+            print(f"Examen le {i["jj"]}/y:{i["mm"]}/{i["aaaa"]}. Info: {i["info"]}")
+        lecturetxt.close
+        lecturecsv.close
+    except FileNotFoundError:
+        print("Fichiers non trouvé")
+        
+
+
+
 
