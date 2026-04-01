9a99766406a19051d97fbc94bd205e6bcd733fa5
Felix-OlivierR <felixo.rioux@gmail.com>
2026-04-01 11:20:26 -0400

    Finition du projet 2

--- Félix-Olivier_R/Projet 2/FONCTIONSP2.py
+++ Félix-Olivier_R/Projet 2/FONCTIONSP2.py
@@ -5,15 +5,70 @@ from time import sleep
 Projet 2 Lecture et écriture de fichiers : FICHIER DE FONCTIONS :)
 Félix-Olivier rioux
 '''
+def lirecours(cours):
+    try: 
+        listecours = open(f"{cours}.txt","r")
+        fichier=listecours.readlines()
+        for ligne in fichier:
+            classe=ligne.split(",")
+            liste=set()
+            for cours in classe:
+                liste.add(cours)
+        listecours.close()
+        try:
+            print(liste)
+        except:
+            print("Aucun cours trouvé")
+        x=0
+    except FileNotFoundError:
+        print("Aucun cours trouvé")
+        x=1
+        return x
 
+def creationfichier(cours,log):
+    delete=input("Effacer la liste de cours? Cela n'efface pas vos notes.\ny/n\n")
+    if delete=="y":
+        try:
+            fichier=open(f"{cours}.txt","w")
+            fichier.write("Liste de vos cours,")
+            fichier.close()
+            print("Liste effacé")
+        except FileNotFoundError:
+            pass
+    lirecours(cours)
+    while True:
+        time=datetime.datetime.now()
+        nom=input("Ajouter un cours? ")
+        if nom=="":
+            break
+        while True:
+            try:
+                logs = open(f"{log}.txt", "x")
+                logs.close()
+                logs = open(f"{log}.txt", "w")
+                logs.write(f"[Created file - {log} - ; {time}]\n")
+                logs.close()
+            except FileExistsError: 
+                logs = open(f"{log}.txt", "a")
+                logs.write(f"[- {nom} - was added to {cours}.txt ; {time}]\n")
+                logs.close()
+                try:
+                    fichier=open(f"{cours}.txt","x")
+                    fichier.close()
+                except FileExistsError:
+                    fichier=open(f"{cours}.txt","a")
+                    fichier.write(f"{nom},")
+                    fichier.close()
+                    lirecours(cours)
+                    break
 
 def Document(append,file,log):
     '''
     Entrée: append = info choisi par l'utilisateur. file = choisir le fichier où écrire. log = choisir où logger l'info
-    Sortie: Dans le fichié chooisi, ajout de ligne avec l'info et le datetime
+    Sortie: Dans le fichié choisi, ajout de ligne avec l'info et le datetime
     '''
-    time=datetime.datetime.now()
     while True:
+        time=datetime.datetime.now()
         try:
             logs = open(f"{log}.txt", "x")
             logs.close
@@ -29,11 +84,11 @@ def Document(append,file,log):
                     fichier.close
                 except FileExistsError:
                     fichier=open(f"{file}","a")
-                    fichier.write(f"{append}    -   ")
+                    fichier.write(f"{append},")
                     break
             break
-    fichier.close
-    logs.close
+    fichier.close()
+    logs.close()
 
 def dateexam(cours,date,info,log):
     '''
@@ -41,23 +96,23 @@ def dateexam(cours,date,info,log):
     Sortie: Dans le fichier rattaché écrire l'information de la date et info, alongside rentrer les log dans le fichier log
     But: Stocker dans un fichier CSV l'information entrée par l'utilisateur pour l'utiliser comme dictionnaire quand on désire print.
     '''
-    time=datetime.datetime.now()
     while True:
+        time=datetime.datetime.now()
         try:
             logs = open(f"{log}.txt", "x")
-            logs.close
+            logs.close()
         except FileExistsError: 
             while True:
                 try: 
                     fichier=open(f"{cours}.csv","x")
-                    fichier.close
+                    fichier.close()
                 except FileExistsError:
                         logs = open(f"{log}.txt", "a")
-                        logs.write(f"[In {cours}.csv added {date},{info} ; {time}]\n")
-                        logs.close
+                        logs.write(f"[In {cours}.csv added {date}, {info} ; {time}]\n")
+                        logs.close()
                         fichier=open(f"{cours}.csv","a")
                         fichier.write(f"{date},{info}\n")
-                        fichier.close
+                        fichier.close()
                         break
         break
                 
@@ -71,7 +126,7 @@ def lecture(cours):
     try:
         lecturetxt=open(f"{cours}.txt","r")     
         print(f"\nNotes ----------------------------\n {lecturetxt.readlines()}")
-        lecturetxt.close
+        lecturetxt.close()
     except FileNotFoundError:
         print(f"\nNotes ----------------------------\nAucunes notes trouvées")
     try: 
@@ -91,7 +146,7 @@ def lecture(cours):
         print("Examens --------------------------")
         for i in dateinfo:
             print(f"{i["jj"]}/{i["mm"]}/{i["aaaa"]}. \nInfo: {i["info"]}\n")
-        lecturecsv.close
+        lecturecsv.close()
     except FileNotFoundError:
         print("Examens --------------------------\nAucun examen à venir")
         
