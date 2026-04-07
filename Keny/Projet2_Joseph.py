92b97521a2a6e967ff8dd9accb67f6bda100578d
kenyboka <keny.boka1@gmail.com>
2026-04-07 19:12:46 -0400

    Update Projet2_Joseph.py

--- Keny/Projet2_Joseph.py
+++ Keny/Projet2_Joseph.py
@@ -2,24 +2,32 @@
 Gestion de budget
 Auteur: Joseph Boka
 """
-Liste_depenses = {
-    "Logement" : 1250,
-    "Hydro" : 170,
-    "Epicerie" : 500,
-    "Communication" : 100,
-    "Carburant" : 200,
-    "assurance" : 200,
-    "Garderie" : 300,
-    "Loisir" : 500,
-    "Imprevus" : 500
-}
-for element in Liste_depenses:
-    print(f"{element},{Liste_depenses[element]}$")
-    Liste_depenses[element]
-#calcul du montant total
-Total = 0 
-for montant in Liste_depenses.values():
-    Total += montant
-    print(f"montant total : {Total}$")
+import csv 
+ 
+def lire_liste_depense(liste_depenses.csv):
+    """
+    lire un fichier CSV
+    afficher chaque dépense
+    calculer le total
+    """
+    Liste_depenses = {}
+    try:
+        fichier = open("liste_depenses.csv", "r")
+        lecteur = csv.reader(fichier)
 
-#Calcul du pourcentage du montant
+        for element in Liste_depenses:
+            print(f"{element},{Liste_depenses[element]}$")
+        Liste_depenses[element]
+        fichier.close()
+
+        #calcul du montant total
+        Total = 0 
+        for montant in Liste_depenses.values():
+            Total += montant
+        print(f"montant total : {Total}$")
+    except FileNotFoundError:
+        print("Erreur : fichier introuvable")
+
+    return liste_depenses
+
+
